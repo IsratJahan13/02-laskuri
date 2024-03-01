01e08b1620ed2a79e9e46fed3a0250c98da03fe6
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-01 14:46:12 +0200

    button is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ const App = () => {
   const [counter, setCounter] = useState(0) //When the state modifying function setCounter is called,
 // React re-renders the component which means that the function body of the component function gets re-executed
 
-  setTimeout(
-    () => setCounter(counter + 1), 1000
-  )
-  //Every time the setCounter modifies the state it causes the component to re-render. 
-  //The value of the state will be incremented again after one second, and this will continue to repeat for as long as the application is running.
-  console.log('rendering...', counter)
-
+const handleClick = () => {
+  console.log('clicked')
+}
   return (
     <>
       {counter}
+      <button onClick={handleClick}>Plus</button>
     </>
   )
 }
