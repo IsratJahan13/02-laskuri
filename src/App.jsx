a35653cf144faec2b6e8f019f9e3afc6ede3ead9
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-01 14:51:14 +0200

    changing the value by button click

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ const App = () => {
   const [counter, setCounter] = useState(0) //When the state modifying function setCounter is called,
 // React re-renders the component which means that the function body of the component function gets re-executed
 
-const handleClick = () => {
-  console.log('clicked')
-}
   return (
     <>
       {counter}
-      <button onClick={handleClick}>Plus</button>
+      <button onClick={() => setCounter(counter + 1)}>Plus</button>
     </>
   )
 }
