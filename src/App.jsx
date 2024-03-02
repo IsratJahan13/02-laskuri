598fbe385c1eba005449f7bfaade4e41f025b55e
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-03 01:13:28 +0200

    some console.log added to understand the process

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,21 @@ const Button = (props) => {
 const App = () => {
   const [counter, setCounter] = useState(0) //When the state modifying function setCounter is called,
 // React re-renders the component which means that the function body of the component function gets re-executed
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <>
@@ -26,7 +38,6 @@ const App = () => {
         onClick = {increaseByOne} 
         text = 'Plus'
       />
-      <button onClick={increaseByOne}>Plus</button>
       <Button 
         onClick = {setToZero}
         text = 'zero'
