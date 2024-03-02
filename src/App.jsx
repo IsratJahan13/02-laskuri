d960139944eda9f5b7cc2931ac4401d1cf27f2c8
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-02 23:50:57 +0200

    minus button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,35 @@ const Display = (props) => {
   )
 }
 
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0) //When the state modifying function setCounter is called,
 // React re-renders the component which means that the function body of the component function gets re-executed
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <>
       <Display counter={counter}/>
+      <Button 
+        onClick = {increaseByOne} 
+        text = 'Plus'
+      />
       <button onClick={increaseByOne}>Plus</button>
-      <button onClick={setToZero}>Zero</button>
-
+      <Button 
+        onClick = {setToZero}
+        text = 'zero'
+      />
+      <Button 
+        onClick = {decreaseByOne} 
+        text = 'Minus'
+      />
     </>
   )
 }
